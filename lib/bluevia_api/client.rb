4fb7833dbb342866f89d637761f466979772be32
Chris Mytton <chrism@simpleweb.co.uk>
2011-08-23 12:25:42 +0100

    sms tests

--- lib/bluevia_api/client.rb
+++ lib/bluevia_api/client.rb
@@ -1,3 +1,5 @@
+require 'oauth'
+
 module BlueviaApi
   class Client
     include Sms
--- test/bluevia_test.rb
+++ test/bluevia_test.rb
@@ -13,4 +13,13 @@ class BlueviaTest < Test::Unit::TestCase
     assert_equal 'consumer_token', BlueviaApi.token
     assert_equal 'consumer_secret', BlueviaApi.secret
   end
+
+  def test_sending_sms
+    client = BlueviaApi.new(
+      :access_token => 'token',
+      :access_token_secret => 'secret'
+    )
+
+    assert_equal true, client.send_sms('00000000000', 'Hello, world!')
+  end
 end
