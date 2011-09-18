933501e71c93b1e84a6ad497759d05e5070d747e
Chris Mytton <self@hecticjeff.net>
2011-09-18 22:28:18 +0100

    test proxying to client

--- spec/bluevia_spec.rb
+++ spec/bluevia_spec.rb
@@ -13,4 +13,10 @@ describe BlueviaApi do
     BlueviaApi.token.should == 'consumer_token'
     BlueviaApi.secret.should == 'consumer_secret'
   end
+
+  describe ".new" do
+    it "returns a BlueviaApi::Client" do
+      BlueviaApi.new.should be_a BlueviaApi::Client
+    end
+  end
 end
